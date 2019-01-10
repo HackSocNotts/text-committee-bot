e6fdb6c0680cfbfb3fe698b14d458f6d53d97a54
Aaron Osher <aaron@oshers.com>
2019-01-10 18:55:32 +0000

    Add config file

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const NEXMO_API = process.env.NEXMO_API;
+export const NEXMO_SECRET = process.env.NEXMO_SECRET;
+export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+export const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
+export const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
