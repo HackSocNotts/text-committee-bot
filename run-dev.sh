8c66f67cc0cec0d6665bb45d6d1b3990ad849506
Aaron Osher <aaron@oshers.com>
2019-01-11 23:32:07 +0000

    Update run-dev.sh Use livedev tag Remove dangling on destroy

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t hacksoc/text-committee-bot .
-docker run --env-file .env --rm -it -v `pwd`/volumes/database/:/usr/app/database/ hacksoc/text-committee-bot:latest
+docker build -t hacksoc/text-committee-bot:livedev .
+docker run --env-file .env --rm -it -v `pwd`/volumes/database/:/usr/app/database/ hacksoc/text-committee-bot:livedev
+docker rmi \`docker images -qa -f 'dangling=true'\`
