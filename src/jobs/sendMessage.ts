494d4f7cb8f6530a22363c90ed84749d8d45f273
Aaron Osher <aaron@aaronosher.io>
2019-04-18 13:21:49 +0100

    Update reject subject

--- src/jobs/sendMessage.ts
+++ src/jobs/sendMessage.ts
@@ -2,7 +2,7 @@ import send from '../mailgun/send';
 
 const FROM = 'HackSoc Job Listings <no-reply@hacksoc.net>';
 const APPROVE_SUBJECT = 'Your job listing has been approved';
-const REJECT_SUBJECT = 'You job listing has been rejected';
+const REJECT_SUBJECT = 'Unfortynatley, You job listing has not been approved';
 const APPROVE_MESSAGE = "We're pleased to inform you that your job listing has been approved, and has been shared with our members.";
 const REJECT_MESSAGE = "We regret to inform you that we have decided that your job listing is not suitable to share with our members, and have opted not to distribute it.";
 const SIGNATURE = "Kind Regards,\nThe HackSoc Team\ninfo@hacksocnotts.co.uk\nhacksocnotts.co.uk";
